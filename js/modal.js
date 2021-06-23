c6ab68067bebb820a706ac84fe22b94ba5d5f5ae
Saharok0707 <saharok0707@gmail.com>
2021-06-23 22:09:42 +0300

    homework6

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,21 @@
-(() => {
+;(() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-  };
+  }
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtns.forEach((btn) => {
+    btn.addEventListener('click', toggleModal)
+  })
+  refs.closeModalBtn.addEventListener('click', toggleModal)
+  window.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      toggleModal()
+    }
+  })
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('is-hidden')
   }
-})();
+})()
